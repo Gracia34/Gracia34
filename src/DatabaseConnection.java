ada1e73b36795229278a58ebd85366f75022ffd2
Gracia34 <20sw515@esisalama.org>
2024-07-25 17:16:51 +0200

    ATELIER_5

---
+++ src/DatabaseConnection.java
@@ -0,0 +1,24 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class DatabaseConnection {
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/parcvehicule"
+    ;
+    private static final String USER =
+            "root"
+    ;
+    private static final String PASSWORD =
+            ""
+    ;
+    public static void main(String[] args) {
+        try {
+            Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            if (connection != null) {
+                System.out.println("Connected to the database!");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
